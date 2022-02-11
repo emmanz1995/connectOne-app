74851ccdb8a140664591061a7e0303ddb6cf09b3
emmanz95 <emmanza2@gmail.com>
2022-02-11 20:51:52 +0000

    new changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import './navbar.scss';
 
 const Navbar = () => {
     return (
-        <div></div>
+        <div className="navbar">
+            <h1 className="navbar__title">ConnectOne</h1>
+            <ul className="navbar__list">
+                <li><a href="/" className="navbar__link">Home</a></li>
+                <li><a href="/" className="navbar__link">Add</a></li>
+                <li><a href="/" className="navbar__link">Save</a></li>
+            </ul>
+        </div>
     );
 }
 
--- client/src/pages/landing/LandingPage.jsx
+++ client/src/pages/landing/LandingPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Navbar from "../../components/Navbar/Navbar";
 
 function LandingPage() {
-    return <h1>Landing</h1>
+    return (
+        <>
+            <Navbar />
+            <h1>Landing</h1>
+        </>
+    )
 }
 
 export default LandingPage;
